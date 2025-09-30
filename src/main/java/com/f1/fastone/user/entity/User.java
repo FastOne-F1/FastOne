0142ac8ccc5dcbae8fe1f1d1e41b8319beae9022
Dayun Kim <ozsally0201@gmail.com>
2025-09-30 11:38:17 +0900

    feat: User 엔티티 username을 PK로 지정

--- src/main/java/com/f1/fastone/user/entity/User.java
+++ src/main/java/com/f1/fastone/user/entity/User.java
@@ -17,11 +17,7 @@ import lombok.*;
 @Table(name = "p_user")
 public class User extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "member_id")
-    private Long id;
-
-    @Column(nullable = false, length = 100, unique = true)
+    @Column(nullable = false, length = 100)
     private String username;
 
     @Column(length = 100)
