0fd1cd9e21cad4fb283e4b115f6b16ce5426b609
owzl <alliwannado1211@gmail.com>
2025-10-01 19:37:46 +0900

    feat: 가게 카테고리 기능 구현

---
+++ src/main/java/com/f1/fastone/store/dto/request/StoreCategoryCreateRequestDto.java
@@ -0,0 +1,17 @@
+package com.f1.fastone.store.dto.request;
+
+import com.f1.fastone.store.entity.StoreCategory;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record StoreCategoryCreateRequestDto(
+        @NotBlank(message = "StoreCategory 이름은 필수입니다.")
+        @Size(max = 80, message = "StoreCategory 이름은 80자를 초과할 수 없습니다.")
+        String storeCategoryName
+) {
+    public StoreCategory toStoreCategoryEntity() {
+        return StoreCategory.builder()
+                .storeCategoryName(this.storeCategoryName)
+                .build();
+    }
+}
--- src/main/java/com/f1/fastone/store/entity/StoreCategory.java
+++ src/main/java/com/f1/fastone/store/entity/StoreCategory.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "p_store_category")
@@ -12,10 +14,17 @@ public class StoreCategory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 80)
+    @Column(nullable = false, length = 80, unique = true)
     private String storeCategoryName;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "store_id", nullable = false)
-    private Store store;
+    // Store store 필드와 store_id JoinColumn을 제거하여 독립적인 카테고리 엔티티로 복구
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "store_id", nullable = false)
+//    private Store store;
+
+    // StoreCategory 수정용
+    public void updateName(String storeCategoryName) {
+        this.storeCategoryName = storeCategoryName;
+    }
+
 }
