a614d4f4ba3579b66398a707e569a55b7821a459
banasu0723 <banasu0723@gmail.com>
2025-10-02 14:51:46 +0900

    feat: 주소 CUD 작성

---
+++ src/main/java/com/f1/fastone/user/repository/UserAddressRepository.java
@@ -0,0 +1,19 @@
+package com.f1.fastone.user.repository;
+
+import com.f1.fastone.user.entity.UserAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserAddressRepository extends JpaRepository<UserAddress, Long> {
+
+    // 사용자의 모든 주소 조회
+    List<UserAddress> findByUserUsernameOrderByCreatedAtDesc(String username);
+
+    // 사용자의 특정 주소 조회
+    Optional<UserAddress> findByIdAndUserUsername(Long id, String username);
+
+    // 사용자의 주소 개수 조회
+    long countByUserUsername(String username);
+}
