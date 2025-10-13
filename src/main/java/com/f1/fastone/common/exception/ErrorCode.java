0634138cf745a4ef2fd4bc184fd5d14c06a3d50f
owzl <134485735+owzl@users.noreply.github.com>
2025-10-13 12:13:24 +0900

    [FEAT] 가게 카테고리 기능(재요청) (#18)
    * feat: 가게 카테고리 기능 구현
    
    * feat: 가게 카테고리 기능 구현
    
    * test: 컨트롤러 & 서비스 테스트 추가

--- src/main/java/com/f1/fastone/common/exception/ErrorCode.java
+++ src/main/java/com/f1/fastone/common/exception/ErrorCode.java
@@ -18,7 +18,9 @@ public enum ErrorCode {
     AUTH_EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED.value(), "A003", "만료된 토큰입니다."),
 
 
-    // store
+    //store
+    STORE_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "S201", "요청한 가게 카테고리를 찾을 수 없습니다."),
+    STORE_CATEGORY_DUPLICATED(HttpStatus.CONFLICT.value(), "S202", "이미 존재하는 가게 카테고리 이름입니다."),
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "S001", "스토어를 찾을 수 없습니다."),
 
     //menu
